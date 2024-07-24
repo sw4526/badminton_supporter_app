a129c43552f8c516a9ed4acfce8888da043643ed
sw4526 <169154264+sw4526@users.noreply.github.com>
2024-07-24 16:26:07 +0900

    std_form

---
+++ app/game_setting_page.js
@@ -0,0 +1,32 @@
+let check1 = document.getElementById("check1");
+let check2 = document.getElementById("check2");
+let check3 = document.getElementById("check3");
+
+function color_change1(){
+    if (check1.style.color == "rgb(255, 255, 255)"){
+        check1.style.color = "rgba(167, 139, 250, 1)"
+        return true;
+    }
+    else{
+        check1.style.color = "fff"
+        return false;
+    }
+} 
+function color_change2(){
+    if (check2.style.color == "rgb(255, 255, 255)"){
+        check2.style.color = "rgba(167, 139, 250, 1)"
+        return true;
+    }
+    else{
+        check2.style.color = "fff"
+        return false;
+    }}
+function color_change3() {
+    if (check3.style.color == "rgb(255, 255, 255)"){
+        check3.style.color = "rgba(167, 139, 250, 1)"
+        return true;
+    }
+    else{
+        check3.style.color = "fff"
+        return false;
+}}
