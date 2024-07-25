06c4151fff447eb03ea1c80d7364bd9f0172bcf7
sw4526 <169154264+sw4526@users.noreply.github.com>
2024-07-25 14:49:27 +0900

    add comment

--- badminton_supporter_app-main/app/main.py
+++ badminton_supporter_app-main/app/main.py
@@ -2,37 +2,50 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#사이트 실행 코드(터미널) : python main.py
+#사이크 클로즈(터미널) : ctrl + c
+#사이트 열기 : 실행후 나오는 주소 ctrl + click
+
+
 @app.route('/') 
 def home():
 	return render_template('main_page.html')
+# 실행시 처음 켜지는 페이지
 
 @app.route('/main_page.html')
 def main_page():
 	return render_template('main_page.html')
-
-@app.route('/log_in_page.html')
-def log_in_page():
-	return render_template('log_in_page.html')
-
-@app.route('/my_page.html')
-def my_page():
-	return render_template('my_page.html')
+# 로그인 하지 않은 상태에서 들어가는 메인 페이지
 
 @app.route('/main_page_login.html')
 def main_page_login():
 	return render_template('main_page_login.html')
+# 로그인한 상태에서 들어가는 메인 페이지
+
+@app.route('/log_in_page.html')
+def log_in_page():
+	return render_template('log_in_page.html')
+#로그인 페이지
+
+@app.route('/my_page.html')
+def my_page():
+	return render_template('my_page.html')
+#마이 페이지(프로필)
 
 @app.route('/game_save_or_not.html')
 def game_save_or_not():
 	return render_template('game_save_or_not.html')
+#마지막 저장 유무 확인 페이지(나중에 실행페이지 만들고 연결해야함)
 
 @app.route('/game_page.html')
 def game_page():
 	return render_template('game_page.html')
+#실행 페이지(구현 안함)
 
 @app.route('/game_setting_page.html')
 def game_setting_page():
 	return render_template('game_setting_page.html')
+#실생 세팅 페이지(나중에 실행페이지 만들고 연결해야함)
 
 if __name__ == '__main__' :   
 	app.run(host='0.0.0.0', port=8000, debug=True)
